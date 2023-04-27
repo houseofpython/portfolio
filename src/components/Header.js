a5934e483775d64d5bc5c7343f679008d54b72ec
‘Diontre <diontre01@gmail.com>
2023-04-27 14:04:04 -0500

    website

--- src/components/Header.js
+++ src/components/Header.js
@@ -28,10 +28,12 @@ class Header extends Component {
   render() {
     if (this.props.sharedData) {
       var name = this.props.sharedData.name;
+      this.description = this.props.sharedData.description;
       this.titles = this.props.sharedData.titles; //...map(x => [ x.toUpperCase(), 1500 ] ).flat();
     }
 
     const HeaderTitleTypeAnimation = React.memo( () => {
+      return this.description
       return this.titles
     }, (props, prevProp) => true);
 
@@ -101,7 +103,7 @@ class Header extends Component {
         <div className="row aligner" style={{height: '100%'}}>
           <div className="col-md-12">
             <div>
-              <span className="iconify header-icon" data-icon="la:laptop-code" data-inline="false"></span>
+              <span className="iconify header-icon"  data-inline="false"></span>
               <br/>
               <h1 className="mb-0">
                 {name}
